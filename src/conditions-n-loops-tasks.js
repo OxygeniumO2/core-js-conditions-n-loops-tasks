9aa5a65a67a47d9d379969f86a996e759d3683e7
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-01-01 10:45:05 +0200

    solve some tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -62,8 +62,12 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x && queen.y === king.y) return true;
+  if (queen.x === king.x || queen.y === king.y) return true;
+  if (queen.x - queen.y === king.x - king.y) return true;
+  if (queen.x + queen.y === king.x + king.y) return true;
+  return false;
 }
 
 /**
@@ -204,8 +208,20 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let firstHalf = '';
+  let secondHalf = '';
+  const middle = Math.floor(str.length / 2);
+  const middleSecondHalf = str.length % 2 === 0 ? middle : middle + 1;
+
+  for (let i = 0; i < middle; i += 1) {
+    firstHalf += str[i];
+  }
+  for (let i = str.length - 1; i >= middleSecondHalf; i -= 1) {
+    secondHalf += str[i];
+  }
+
+  return firstHalf === secondHalf;
 }
 
 /**
@@ -222,8 +238,11 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) return i;
+  }
+  return -1;
 }
 
 /**
@@ -241,8 +260,11 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  for (let i = num; i > 0; i = Math.floor(i / 10)) {
+    if (i % 10 === digit) return true;
+  }
+  return false;
 }
 
 /**
@@ -258,8 +280,21 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  for (let i = 1; i < arr.length; i += 1) {
+    let tempLeft = 0;
+    let tempRight = 0;
+    for (let j = 0; j < i; j += 1) {
+      tempLeft += arr[j];
+    }
+    for (let h = i + 1; h < arr.length; h += 1) {
+      tempRight += arr[h];
+    }
+    if (tempLeft === tempRight) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
